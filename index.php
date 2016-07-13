13cc3a1846772ce3cf2c1292dcc9dc7b2541cf58
naxespsn <x14428372@student.ncirl.ie>
2016-07-13 02:18:28 +0000

    0.9.4

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
         
         <!-- Tab Icon -->
-        <link rel="icon" href="/IMG/tab_icon.jpg"/>
+        <link rel="icon" href="/IMG/tab_icon.png"/>
         
         <!-- Fonts -->
         <link href='https://fonts.googleapis.com/css?family=Quicksand|Cuprum|Amatic+SC|Indie+Flower' rel='stylesheet' type='text/css'>
